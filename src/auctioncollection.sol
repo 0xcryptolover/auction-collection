930072a90bc326c9eedaf215714213fe6d80c00e
helloworld <thach.bao@incognito.org>
2023-03-29 13:39:49 +0700

    tested contract.

--- src/auctioncollection.sol
+++ src/auctioncollection.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
-import "./linkedlist.sol";
 
-contract AuctionCollection is Ownable, LinkedListLib {
+contract AuctionCollection is Ownable {
+    uint256 public constant MAX_WINNERS = 512;
+
+    struct Bidder {
+        bool isWinner;
+        address bidder;
+        string btcAddr;
+        uint256 amount;
+    }
 
     // vars
     uint256 public endTime;
     uint256 public bidMinimum;
-    uint256 public totalUserBid; // tracking purpose
-    uint256 public totalClaimable;
-    mapping(address => uint256) public notEligibleList;
+    uint256 private totalPaymentWithdraw;
+    mapping(string => Bidder) private bidders;
+    bool private winnerDeclared;
+    mapping(string => bool) private isBtcExisted;
+    string[] private btcAddresses;
 
     // events
     event Bid(address, string, uint256);
@@ -24,54 +32,78 @@ contract AuctionCollection is Ownable, LinkedListLib {
     }
 
     //    Bid(btc address, eth amount) // check min bid amount
-    function bid(string calldata btcAddr) external payable {
-        require(bytes(btcAddr).length != 0 && msg.value >= bidMinimum, "AUC: btc address mut be not null and bid amount greater than minimum");
+    function bid(string calldata btcAddr, address bidder) external payable {
         uint256 bidAmount = msg.value;
-        address bidder = msg.sender;
-        if (notEligibleList[bidder] > 0) {
-            bidAmount += notEligibleList[bidder];
-            notEligibleList[bidder] = 0;
+        require(bytes(btcAddr).length != 0 && bidAmount >= bidMinimum && block.number < endTime, "AUC: btc address mut be not null and bid amount greater than minimum");
+        if (bidders[btcAddr].amount == 0) {
+            bidders[btcAddr] = Bidder(false, bidder, btcAddr, bidAmount);
+        } else {
+            bidders[btcAddr].amount += bidAmount;
         }
-        (address removedAddr, uint256 removedAmount) = addNodeSorted(bidder, btcAddr, bidAmount);
-        if (removedAddr != address(0)) {
-            notEligibleList[removedAddr] = removedAmount;
+
+        if (!isBtcExisted[btcAddr]) {
+            btcAddresses.push(btcAddr);
         }
 
         emit Bid(bidder, btcAddr, bidAmount);
     }
 
+    // declare winners
+    function declareWinners(string[] memory winners) external onlyOwner {
+        require(block.number >= endTime,"AUC: auction not end yet");
+        require(winners.length < MAX_WINNERS,"AUC: too many winners");
+        uint256 _totalPaymentWithdraw;
+        for (uint256 i = 0; i < winners.length; i++) {
+            Bidder storage tmpBidder = bidders[winners[i]];
+            require(!tmpBidder.isWinner && tmpBidder.amount > 0, "AUC: duplicate winner or invalid");
+            _totalPaymentWithdraw += tmpBidder.amount;
+            tmpBidder.isWinner = true;
+        }
+        winnerDeclared = true;
+        totalPaymentWithdraw = _totalPaymentWithdraw;
+    }
+
     //    WithdrawPayment()
-    function withdrawPayment() external onlyOwner {
+    function withdrawPayment(address payable receiver) external onlyOwner {
         require(block.number >= endTime, "AUC: withdraw only after end time");
-        uint256 paymentWithdrawAmount = address(this).balance - totalClaimable;
-        require(paymentWithdrawAmount > 0, "AUC: nothing to withdraw");
-        uint256 amount = paymentWithdrawAmount;
-        paymentWithdrawAmount = 0;
-        (bool success, ) = msg.sender.call{value: amount}("");
+        require(totalPaymentWithdraw > 0 && winnerDeclared, "AUC: nothing to withdraw or winners not declared");
+        uint256 amount = totalPaymentWithdraw;
+        totalPaymentWithdraw = 0;
+        (bool success, ) = receiver.call{value: amount}("");
         require(success, "AUC: failed to withdraw");
     }
 
     //    Refund()
-    function refund() external {
+    function refund(string calldata btcAddr) external {
         // the auction must end to be able claim eth back
-        require(block.number >= endTime, "AUC: withdraw only after end time");
-        uint256 refundAmount = notEligibleList[msg.sender];
-        require(refundAmount > 0, "AUC: nothing to refund");
-        notEligibleList[msg.sender] = 0;
-        (bool success, ) = msg.sender.call{value: 0}("");
+        require(block.number >= endTime && winnerDeclared, "AUC: withdraw only after end time and winner declared");
+        require(!bidders[btcAddr].isWinner, "AUC: must be not a winner");
+
+        uint256 refundAmount = bidders[btcAddr].amount;
+        bidders[btcAddr].amount = 0;
+        (bool success, ) = bidders[btcAddr].bidder.call{value: refundAmount}("");
         require(success, "AUC: failed to refund");
 
         emit Refund(msg.sender, refundAmount);
     }
 
-    //    ListBids()
-    function listBids() external returns(uint[] memory) {
-        uint[] memory temp = new uint[](0);
+    // get total bids
+    function totalBids() external view returns(uint256) {
+        return btcAddresses.length;
+    }
+
+    // ListBids()
+    function listBids(uint256 start, uint256 end) external view returns(Bidder[] memory) {
+        require(end < btcAddresses.length, "AUC: invalid index");
+        Bidder[] memory temp = new Bidder[](end - start);
+        for (uint i = start; i < end; i++) {
+            temp[i] = bidders[btcAddresses[i]];
+        }
         return temp;
     }
 
-    //    GetBidsByAddress()
-    function getBidsByAddress() external returns(uint256) {
-        return 0;
+    //  GetBidsByAddress()
+    function getBidsByAddress(string calldata btcAddr) external view returns(Bidder memory) {
+        return bidders[btcAddr];
     }
 }
--- test/auctioncollection.t.sol
+++ test/auctioncollection.t.sol
@@ -6,6 +6,7 @@ import "../src/auctioncollection.sol";
 
 contract AuctionCollectionTest is Test {
     AuctionCollection public ac;
+    event Refund(address, uint256);
 
     function setUp() public {
         ac = new AuctionCollection(1000, 1e10);
@@ -13,14 +14,100 @@ contract AuctionCollectionTest is Test {
 
     function testBids() public {
         address user;
+
         string memory btcAddr;
         for (uint i = 1; i < 522; i ++) {
             btcAddr = numberToString(i);
             user = address(uint160(i));
             vm.prank(user);
             vm.deal(user, i * 1e10);
-            ac.bid{value: i * 1e10}(btcAddr);
+            ac.bid{value: i * 1e10}(btcAddr, user);
         }
+        assertEq(ac.totalBids(), 521);
+        AuctionCollection.Bidder memory tmp = ac.getBidsByAddress(numberToString(1));
+        assertEq(tmp.btcAddr, numberToString(1));
+        assertEq(tmp.bidder, address(uint160(1)));
+        assertEq(tmp.amount, 1e10);
+
+        user = address(uint160(1));
+        vm.prank(user);
+        vm.deal(user, 1e10);
+        vm.expectRevert("AUC: btc address mut be not null and bid amount greater than minimum");
+        ac.bid{value: 1e10}("", user);
+
+        vm.prank(user);
+        ac.bid{value: 1e10}(numberToString(1), user);
+
+        tmp = ac.getBidsByAddress(numberToString(1));
+        assertEq(tmp.btcAddr, numberToString(1));
+        assertEq(tmp.bidder, address(uint160(1)));
+        assertEq(tmp.amount, 2e10);
+
+        // roll to end time
+        vm.roll(1001);
+        user = address(uint160(523));
+        vm.prank(user);
+        vm.deal(user, 1e10);
+        vm.expectRevert("AUC: btc address mut be not null and bid amount greater than minimum");
+        ac.bid{value: 1e10}(numberToString(523), user);
+
+        // declare winners
+        uint256 verifyWithdrawAmount = 1e10;
+        string[] memory winners = new string[](100);
+        for (uint i = 0; i < 100; i ++) {
+            winners[i] = numberToString(i+1);
+            verifyWithdrawAmount += (i + 1) * 1e10;
+        }
+        vm.prank(user);
+        vm.expectRevert("Ownable: caller is not the owner");
+        ac.declareWinners(winners);
+
+        winners[0] = numberToString(0);
+        vm.expectRevert("AUC: duplicate winner or invalid");
+        ac.declareWinners(winners);
+
+        winners[0] = numberToString(1);
+        winners[1] = numberToString(1);
+        vm.expectRevert("AUC: duplicate winner or invalid");
+        ac.declareWinners(winners);
+
+        winners[1] = numberToString(2);
+        ac.declareWinners(winners);
+
+        tmp = ac.getBidsByAddress(numberToString(1));
+        assertEq(tmp.isWinner, true);
+
+        // withdraw payments
+        address payable receiver = payable(address(uint160(1000)));
+        vm.prank(user);
+        vm.expectRevert("Ownable: caller is not the owner");
+        ac.withdrawPayment(receiver);
+
+        ac.withdrawPayment(receiver);
+        assertEq(verifyWithdrawAmount, receiver.balance);
+
+        // request refund
+        for (uint i = 101; i < 522; i ++) {
+            btcAddr = numberToString(i);
+            user = address(uint160(i));
+            vm.prank(user);
+            vm.expectEmit(false, false, false, true);
+            emit Refund(user, i * 1e10);
+            ac.refund(btcAddr);
+            assertEq(user.balance, i * 1e10);
+        }
+
+        // user request refund many times
+        btcAddr = numberToString(101);
+        user = address(uint160(101));
+        vm.prank(user);
+        vm.expectEmit(false, false, false, true);
+        emit Refund(user, 0);
+        ac.refund(btcAddr);
+        assertEq(user.balance, 101 * 1e10);
+
+        // test get functions
+        ac.listBids(0, 100);
     }
 
     /**
